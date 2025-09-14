f768c583d0db8951d4f306bf93ec39f3f4e231f9
itsmanojdev <manojscmr333@gmail.com>
2025-09-14 14:24:36 +0530

    Side bar and Node list

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -1,13 +1,72 @@
-import type { NodeType } from "../types";
+import { useCallback } from "react";
+import { useDnD, useDnDPosition, type OnDropAction } from "../hooks/useDnD";
+import { useReactFlow, type XYPosition } from "@xyflow/react";
+import { nodeTypes } from "../constants";
+import type { nodeDataType } from "../types";
+import { getDefaultNodeData } from "../utils";
 
-const Node = ({ icon: Icon, name }: NodeType) => {
+let id = 1;
+const getId = () => `node_${id++}`;
+
+const Node = ({ type: nodeType }: { type: string }) => {
+    const { onDragStart, isDragging } = useDnD();
+    const { setNodes } = useReactFlow();
+
+    const nodeData: nodeDataType = getDefaultNodeData(nodeType);
+
+    const createAddNewNode = useCallback(
+        (nodeType: string): OnDropAction => {
+            return ({ position }: { position: XYPosition }) => {
+                const id = getId();
+                const newNode = {
+                    id: id,
+                    type: nodeType,
+                    position,
+                    data: nodeData,
+                };
+
+                setNodes((nds) => nds.concat(newNode));
+            };
+        },
+        [setNodes],
+    );
+
+    const { icon: Icon, name } = nodeTypes[nodeType];
 
     return (
-        <div className='text-indigo-500 h-20 flex flex-col p-1 justify-center items-center border-2 border-indigo-500 rounded-sm'>
-            <Icon />
-            <p className="mt-1">{name}</p>
-        </div>
+        <>
+            {isDragging && <DragGhost type={nodeType} />}
+            <div
+                className='text-indigo-500 h-20 flex flex-col p-1 justify-center items-center border-2 border-indigo-500 rounded-sm'
+                onPointerDown={(event) => {
+                    onDragStart(event, createAddNewNode(nodeTypes.MESSAGE.key));
+                }}
+            >
+                <Icon />
+                <p className="mt-1">{name}</p>
+            </div>
+        </>
     )
 }
 
+
+export function DragGhost({ type: nodeType }: { type: string }) {
+    const { position } = useDnDPosition();
+    const { icon: Icon, name } = nodeTypes[nodeType];
+
+    if (!position) return null;
+
+    return (
+        <div
+            className='fixed pointer-events-none text-indigo-500 h-20 w-40 flex flex-col p-1 justify-center items-center border-2 border-indigo-500 rounded-sm'
+            style={{
+                left: `${position.x}px`,
+                top: `${position.y}px`,
+            }}
+        >
+            <Icon />
+            <p className="mt-1">{name}</p>
+        </div>
+    );
+}
 export default Node
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,24 @@
-import { nodeTypes } from "../data";
+import { IconContext } from "react-icons";
+import { nodeTypes } from "../constants";
 import Node from "./Node";
+import { useChatFlow } from "../contexts/ChatFlowContext";
+import NodeEditView from "./NodeEditView";
+
 
 const Sidebar = () => {
+    const chatFlowProps = useChatFlow();
+
     return (
-        <div className="grid grid-cols-2 gap-4 p-4">
-            {nodeTypes.map((node, index) => <Node key={index} icon={node.icon} name={node.name} />)}
-        </div>
+        <>
+            {chatFlowProps?.selectedNodeId
+                ? <NodeEditView />
+                : (<IconContext.Provider value={{ size: '24px' }}>
+                    <div className="grid grid-cols-2 gap-4 p-4">
+                        {Object.keys(nodeTypes).map((node, idx) => <Node key={idx} type={node} />)}
+                    </div>
+                </IconContext.Provider>)
+            }
+        </>
     )
 }
 
